f942c9fdade6bd958fcd55fd3e90052462cfe4ba
Dream-test <stsiapkou@gmail.com>
2024-11-16 21:20:14 +0100

    Created class Circle

--- src/com/example/circle/Circle.java
+++ src/com/example/circle/Circle.java
@@ -4,5 +4,30 @@ package com.example.circle;
  * Заполните этот класс в соответсвии с заданием из лекции.
  */
 public class Circle {
+    private double radius;
 
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return this.radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = Math.max(radius, 0);
+    }
+
+    public double getArea() {
+        if (this.radius > 0) {
+            return Math.PI*Math.pow(this.radius, 2);
+        } else return 0;
+    }
+
+    public double getAreaByRadius(double radius) {
+        if (radius > 0) {
+            return Math.PI * Math.pow(radius, 2);
+        }
+        return 0;
+    }
 }
